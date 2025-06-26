f22e1a09b1bb8dbc1089cef421f71c7d79a74622
Joao Gabriel <123424925+artesssn@users.noreply.github.com>
2025-06-25 22:47:18 -0300

    Add files via upload

---
+++ E2/E2/Produto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace classeProduto
+{
+    public class Produto
+    {
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public static List<Produto> Produtos = new List<Produto>();
+
+        public void CadastrarProduto()
+        {
+            Produto novoProduto = new Produto();
+            Console.WriteLine("\nVamos cadastrar um produto...");
+            Console.WriteLine("Digite a descrição do produto: ");
+            novoProduto.Descricao = Console.ReadLine();
+            Console.WriteLine("Digite o valor do produto: ");
+            novoProduto.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Produtos.Add(novoProduto);
+            Console.WriteLine("Produto cadastrado com sucesso!");
+        }
+        public void RemoverProduto()
+        {
+            Console.WriteLine("Digite a descrição do produto que deseja remover: ");
+            string descricao = Console.ReadLine();
+            Produto produtoEncontrado = Produtos.FirstOrDefault(a => a.Descricao == descricao);
+
+            if (produtoEncontrado != null)
+            {
+                Produtos.Remove(produtoEncontrado);
+                Console.WriteLine("Produto removido com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Produto não encontrado.");
+            }
+        }
+        public void PesquisarProduto()
+        {
+            Console.WriteLine("Digite a descrição do produto que deseja pesquisar: ");
+            string descricao = Console.ReadLine();
+            Produto produtoEncontrado = Produtos.FirstOrDefault(a => a.Descricao == descricao);
+
+            if (produtoEncontrado != null)
+            {
+                Console.WriteLine("\n---- DETALHES DO PRODUTO: ----");
+                Console.WriteLine($"Descrição do produto: {produtoEncontrado.Descricao} \nValor: R${produtoEncontrado.Valor:F2}");
+
+            }
+            else
+            {
+                Console.WriteLine("Produto não encontrado.");
+            }
+        }
+        public void ProdutoMenorValor()
+        {
+            double menorValor = double.MaxValue;
+            Produto produtoMenor = null;
+            for (int i = 0; i < Produtos.Count; i++)
+            {
+                if (Produtos[i].Valor < menorValor)
+                {
+                    menorValor = Produtos[i].Valor;
+                    produtoMenor = Produtos[i];
+                }
+            }
+            if (produtoMenor != null)
+            {
+                Console.WriteLine($"Produto com o menor valor: {produtoMenor.Descricao}");
+                Console.WriteLine($"Valor: R${produtoMenor.Valor:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Nenhum produto encontrado.");
+            }
+
+        }
+    }
+}
---
+++ E2/E2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using classeProduto;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto _produto = new Produto();
+            Console.WriteLine("\t BEM VINDO AO SISTEMA DE GERENCIAMENTO DE PRODUTO");
+            bool executando = true;
+            while (executando)
+            {
+
+                Console.WriteLine("\nDigite o número correspondente ao que deseja realizar: ");
+                Console.WriteLine("1- Cadastrar um produto");
+                Console.WriteLine("2- Remover produto");
+                Console.WriteLine("3- Pesquisar produto");
+                Console.WriteLine("4- Encontrar produto de menor valor");
+                Console.WriteLine("5- Sair do sistema");
+
+                int opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        _produto.CadastrarProduto();
+                        break;
+                    case 2:
+                        _produto.RemoverProduto();
+                        break;
+                    case 3:
+                        _produto.PesquisarProduto();
+                        break;
+                    case 4:
+                        _produto.ProdutoMenorValor();
+                        break;
+                    case 5:
+                        executando = false;
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Digite um número de 1 a 5.");
+                        break;
+                }
+                Console.WriteLine("\n Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+        }
+    }
+}
